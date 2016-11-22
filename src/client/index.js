3bb4bb6ff261b660547662a8811f9bf431f42ff4
Krzysztof Gardocki <krzysztof.gardocki@gmail.com>
2016-11-22 15:12:59 +0100

    Eslint works pretty well

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom'
 
 import App from './App'
 
-import style from './styles/main.scss'
+import style from './styles/main.scss' // eslint-disable-line no-unused-vars
 
 render(<App />, document.getElementById('app'));
